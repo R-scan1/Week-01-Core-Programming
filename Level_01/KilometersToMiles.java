ec9998ffeb941d88c699918e0211cc1c720679f3
R-scan1 <deepr7590@gmail.com>
2025-01-11 10:36:46 +0530

    Pushing code_10

---
+++ Level_01/KilometersToMiles.java
@@ -0,0 +1,13 @@
+public class KilometersToMiles{
+    public static void main(String [] args){
+		
+	//declaring and initializing the variable 
+		double distanceInKM=10.4;
+		
+	//calaculating distance in miles 1km=1.6 miles
+		double inMiles=10.4*1.6;
+		
+	//displaying distance in miles
+         System.out.println("The distance "+distanceInKM+"km in miles is "+inMiles);
+	}
+}
