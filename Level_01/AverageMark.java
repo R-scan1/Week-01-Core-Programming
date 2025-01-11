348fe4d9901a2360fdf71dc89a4d6cc6a8786d48
R-scan1 <deepr7590@gmail.com>
2025-01-11 10:32:47 +0530

    Pushing code_2

---
+++ Level_01/AverageMark.java
@@ -0,0 +1,15 @@
+public class AverageMark{
+      public static void main(String [] args){
+		  
+		//declaring variables and assigning subject marks
+		  int maths=94;
+		  int physics=95;
+		  int chemistry=96;
+		  
+		//calculating average marks
+		  double average=(double)(maths+physics+chemistry)/3;   //typecasting 
+		  
+		//displaying the average marks  
+		  System.out.println("Sam’s average mark in PCM is "+average);
+	  }
+}
