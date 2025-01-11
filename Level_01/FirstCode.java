6bf8b8200e694e3999fb23937b6fc91d66c15c62
R-scan1 <deepr7590@gmail.com>
2025-01-11 10:34:21 +0530

    Pushing code_5

---
+++ Level_01/FirstCode.java
@@ -0,0 +1,15 @@
+public class FirstCode{
+     public static void main(String [] args){
+		 
+	//initializing and declaring variables
+	  int currentYear=2024;
+	  int birthYear=2000;
+	  
+	//calculating the age
+	  int age=currentYear-birthYear;
+	  
+	//displaying age   
+	  System.out.println("Harry's age in "+currentYear+" is "+age);
+	  
+	 }
+}
